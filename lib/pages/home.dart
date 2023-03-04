bbc8ce7ada7cd94a2846da22d1ab742e4db3f963
prashant-cyberarrow <prashant.silpakar@cyberarrow.io>
2023-03-04 22:58:35 +0545

    Added flex to home page widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -49,103 +49,113 @@ class _HomeState extends State<Home> {
       body: Container(
         child: Column(
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                _buttonTitles.length,
-                    (index) => ElevatedButton(
-                  onPressed: () {
-                    // Add your onPressed code here.
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                      ),
-                    ),
-                  ),
-                  child: Text(_buttonTitles[index]),
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  onPressed: _decrementDate,
-                  icon: Icon(Icons.arrow_left),
-                ),
-                Text(
-                  DateFormat('EEE, MMM dd').format(_selectedDate),
-                  style: TextStyle(fontSize: 18.0),
-                ),
-                IconButton(
-                  onPressed: _incrementDate,
-                  icon: Icon(Icons.arrow_right),
-                ),
-              ],
-            ),
-
-            //Data table
-            Container(
-              height: 300.0,
-              child: ValueListenableBuilder<Box<CashModel>>(
-                valueListenable: Cashes.getData().listenable(),
-                builder: (BuildContext context, Box<CashModel> box, _) {
-                  final myModels = box.values.toList().cast<CashModel>();
-                  // return SingleChildScrollView(
-                  //   scrollDirection: Axis.horizontal,
-                  //   child: ConstrainedBox(
-                  //     constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width),
-                  //     child: DataTable(
-                  //       columns: [
-                  //         DataColumn(label: Text('Date')),
-                  //         DataColumn(label: Text('Cash In')),
-                  //         DataColumn(label: Text('Cash Out')),
-                  //       ],
-                  //       rows: List<DataRow>.generate(
-                  //         myModels.length,
-                  //             (int index) => DataRow(
-                  //           cells: [
-                  //             DataCell(Text(myModels[index].date.toString())),
-                  //             DataCell(Text(myModels[index].cashIn.toString())),
-                  //             DataCell(Text(myModels[index].cashOut.toString())),
-                  //           ],
-                  //         ),
-                  //       ),
-                  //     ),
-                  //   ),
-                  // );
-
-                  return SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: DataTable(
-                      columns: [
-                        DataColumn(label: Text('Date')),
-                        DataColumn(label: Text('Cash In')),
-                        DataColumn(label: Text('Cash Out')),
-                      ],
-                      rows: List<DataRow>.generate(
-                        myModels.length,
-                            (int index) => DataRow(
-                          cells: [
-                            DataCell(
-                                myModels[index].date == null ? Text('') : Text(DateFormat('yyyy-MM-dd').format(myModels[index].date!))
-                            ),
-                            DataCell(
-                                myModels[index].cashIn == null ? Text('') :Text(myModels[index].cashIn.toString())
-                            ),
-                            DataCell(
-                                myModels[index].cashOut == null ? Text('') :Text(myModels[index].cashOut.toString())
-                            ),
-                          ],
+            Expanded(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: List.generate(
+                  _buttonTitles.length,
+                      (index) => ElevatedButton(
+                    onPressed: () {
+                      // Add your onPressed code here.
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
                         ),
                       ),
                     ),
-                  );
+                    child: Text(_buttonTitles[index]),
+                  ),
+                ),
+              ),
+            ),
 
-                },
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: _decrementDate,
+                    icon: Icon(Icons.arrow_left),
+                  ),
+                  Text(
+                    DateFormat('EEE, MMM dd').format(_selectedDate),
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  IconButton(
+                    onPressed: _incrementDate,
+                    icon: Icon(Icons.arrow_right),
+                  ),
+                ],
+              ),
+            ),
+
+            //Data table
+            Expanded(
+              flex: 12,
+              child: Container(
+                height: 300.0,
+                child: ValueListenableBuilder<Box<CashModel>>(
+                  valueListenable: Cashes.getData().listenable(),
+                  builder: (BuildContext context, Box<CashModel> box, _) {
+                    final myModels = box.values.toList().cast<CashModel>();
+                    // return SingleChildScrollView(
+                    //   scrollDirection: Axis.horizontal,
+                    //   child: ConstrainedBox(
+                    //     constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width),
+                    //     child: DataTable(
+                    //       columns: [
+                    //         DataColumn(label: Text('Date')),
+                    //         DataColumn(label: Text('Cash In')),
+                    //         DataColumn(label: Text('Cash Out')),
+                    //       ],
+                    //       rows: List<DataRow>.generate(
+                    //         myModels.length,
+                    //             (int index) => DataRow(
+                    //           cells: [
+                    //             DataCell(Text(myModels[index].date.toString())),
+                    //             DataCell(Text(myModels[index].cashIn.toString())),
+                    //             DataCell(Text(myModels[index].cashOut.toString())),
+                    //           ],
+                    //         ),
+                    //       ),
+                    //     ),
+                    //   ),
+                    // );
+
+                    return SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: DataTable(
+                        columns: [
+                          DataColumn(label: Text('Date')),
+                          DataColumn(label: Text('Cash In')),
+                          DataColumn(label: Text('Cash Out')),
+                        ],
+                        rows: List<DataRow>.generate(
+                          myModels.length,
+                              (int index) => DataRow(
+                            cells: [
+                              DataCell(
+                                  myModels[index].date == null ? Text('') : Text(DateFormat('yyyy-MM-dd').format(myModels[index].date!))
+                              ),
+                              DataCell(
+                                  myModels[index].cashIn == null ? Text('') :Text(myModels[index].cashIn.toString())
+                              ),
+                              DataCell(
+                                  myModels[index].cashOut == null ? Text('') :Text(myModels[index].cashOut.toString())
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+
+                  },
+                ),
               ),
             ),
 
@@ -177,95 +187,102 @@ class _HomeState extends State<Home> {
             //   ),
             // ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/form');
-                  },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      shape: RoundedRectangleBorder(
+            Expanded(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/form');
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                    ),
+                    child: Text("Cash In"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/cashout');
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20),
                       ),
-                  ),
-                  child: Text("Cash In"),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/cashout');
-                  },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
                     ),
-                  ),
-                  child: Text("Cash Out"),
-                ),
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(5.0, 20.0, 5.0, 10.0),
-              child: Table(
-                border: TableBorder.all(),
-                columnWidths: {
-                  0: FlexColumnWidth(2),
-                  1: FlexColumnWidth(1),
-                  2: FlexColumnWidth(1),
-                },
-                children: [
-                  TableRow(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('Total Cash In'),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('Total Cash Out'),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('Balance'),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(''),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('Previous Balance'),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('500'),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(''),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('Balance'),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('500'),
-                      ),
-                    ],
+                    child: Text("Cash Out"),
                   ),
                 ],
-              )
+              ),
+            ),
+
+            Expanded(
+              flex: 4,
+              child: Container(
+                padding: EdgeInsets.fromLTRB(5.0, 20.0, 5.0, 10.0),
+                child: Table(
+                  border: TableBorder.all(),
+                  columnWidths: {
+                    0: FlexColumnWidth(2),
+                    1: FlexColumnWidth(1),
+                    2: FlexColumnWidth(1),
+                  },
+                  children: [
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('Total Cash In'),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('Total Cash Out'),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('Balance'),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(''),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('Previous Balance'),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('500'),
+                        ),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(''),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('Balance'),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('500'),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ),
             )
           ],
         ),
